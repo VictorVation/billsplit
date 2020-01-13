6107e12c0295582b6a8722cb60aa50252008b080
Victor Szeto <me@victorszeto.com>
2020-01-12 16:13:24 -0800

    Updates

---
+++ precache-manifest.9137b15a53ede401f1aecf286c902386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15264aecfe2422eeef8fd57f758680f",
+    "url": "/billsplit/index.html"
+  },
+  {
+    "revision": "3000f38b64c663322a03",
+    "url": "/billsplit/static/css/main.6d1a01de.chunk.css"
+  },
+  {
+    "revision": "052d2d56facce19a8482",
+    "url": "/billsplit/static/js/2.2ba00c72.chunk.js"
+  },
+  {
+    "revision": "3d2ffa965c98b231022ab4538771c163",
+    "url": "/billsplit/static/js/2.2ba00c72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3000f38b64c663322a03",
+    "url": "/billsplit/static/js/main.52a9d1e3.chunk.js"
+  },
+  {
+    "revision": "ec3ef67b32886df3a5e4",
+    "url": "/billsplit/static/js/runtime-main.21cf5f45.js"
+  }
+]);
